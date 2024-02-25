0a9389deab43fa7ff32270052de3f4c42b727741
Deby Putri <144690329+22091397029DebyPutriSulistyoRini@users.noreply.github.com>
2024-02-25 22:35:19 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Kelompok 8',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('image/Robot.png'),
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    ),
+  );
+}
